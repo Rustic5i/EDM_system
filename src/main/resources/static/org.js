6c50f8acc8ddb66bca1e12e9c31680eb8de72891
Rustic2 <rus5i1819@gmail.com>
2021-09-07 22:17:05 +0500

    1.3(Fix bug List Name Organization  the index.html)

--- src/main/resources/static/org.js
+++ src/main/resources/static/org.js
@@ -1,33 +1,37 @@
 let OrgList = angular.module('OrgList', []) // создаем обьект Angular
 // $scope - это как бд, в него будет сохран€ть все что нам будет нужно. » мы будем получать к нему доступ в вью через скобки {{}}
 OrgList.controller('orgListController', function ($scope, $http) {
-
     $http.get('http://localhost:8080/api/organization').then(resp => {
         $scope.listOrg = resp.data;
         console.log($scope.listOrg)
     }, resp => {
         console.error(resp)
     })
+
     $scope.getDepartByIdOrg = function (id) {
         $http.get('http://localhost:8080/api/organization/' + id + '/department/').then(resp => {
             // нужно пройти циклом, найти  html по айдишкам и заполнить их от сюда
             $scope.listDepartment = resp.data
-            // $scope.test($scope.listDepartment)
+            $scope.listButtonOrganization($scope.listDepartment,id)
             console.log($scope.listDepartment)
         }, resp => {
             console.error(resp)
         })
     }
-    // $scope.test = function (listDepart) {
-    //     const container = $('#idDepart');
-    //     let html = ''
-    //     listDepart.forEach(deport => {
-    //         let buttonHtml = ` <div id="idDepart1" class="card card-body">${deport.id}</div>`
-    //         html += buttonHtml;
-    //         container.html(html);
-    //     })
-    //     console.log(html)
-    // }
+
+
+
+    $scope.listButtonOrganization = function (listDepart,idOrg) {
+        const container = $('#idDepart'+idOrg);
+        console.log(container)
+        let html = ''
+        listDepart.forEach(deport => {
+            let buttonHtml = ` <p id="textNameOrg1">${deport.nameDepartment}</p>`
+            html += buttonHtml;
+            container.html(html);
+        })
+        console.log(html)
+    }
 })
 
 
